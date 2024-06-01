50daf21c2864b699ef29ce83a63b15b2cd85d0ae
YoussefMarzoukk <136881249+YoussefMarzoukk@users.noreply.github.com>
2024-06-02 00:06:35 +0300

    Update CheckOutTest.java

--- src/test/java/com/nopcommerce/demo/TestCases/CheckOutTest.java
+++ src/test/java/com/nopcommerce/demo/TestCases/CheckOutTest.java
@@ -12,18 +12,17 @@ public class CheckOutTest extends BaseTest {
 
     @Test
     public void validCheckout() {
-        // Navigate to the home page and add a product to the cart
         HomePage homePage = new HomePage(driver);
-        homePage.load().setAddProductToCart().cartButton().acceptTerms().proceedToCheckout().GuestCheckout();
+        homePage
+            .load()
+            .setAddProductToCart()
+            .cartButton()
+            .acceptTerms()
+            .proceedToCheckout()
+            .GuestCheckout();
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(200));
-
-        // Initialize the checkout page
         CheckoutPage checkoutPage = new CheckoutPage(driver);
-
-        // Execute the checkout sequence
         checkoutPage.checkOutSequence();
-
-        // Verify that the order was successful
         boolean isSuccessful = checkoutPage.isSuccessful();
         Assert.assertTrue(isSuccessful);
     }}
